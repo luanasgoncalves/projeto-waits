48d088aae0a87c67c6891b860a7621238c58b9bc
Barbara1614 <2026007716@aluno.poa.ifrs.edu.br>
2026-06-09 17:26:43 -0300

    arrumamo o js do carrossel

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -5,11 +5,11 @@ document.getElementById('radio1').checked = true;
 
 // função para definir o tempo que cada imagem fica antes de passar para próxima (5 segundos)
 setInterval(() => {
+    cont++;
     proximaImagem()
 }, 5000);
 
 function proximaImagem() {
-    cont++;
 
     if(cont > 3) {
         cont = 1;
